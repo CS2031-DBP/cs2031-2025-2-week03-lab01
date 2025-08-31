e5d612a23ad1ee6d257a2fcd6e2c36964808fd21
katlimruiz <katlimruiz@gmail.com>
2025-08-31 11:52:38 -0500

    AlbumService must not call the SongRepository directly. It should do it thru the SongService.

--- src/main/java/org/lab/week03lab01/service/AlbumService.java
+++ src/main/java/org/lab/week03lab01/service/AlbumService.java
@@ -3,17 +3,16 @@ package org.lab.week03lab01.service;
 import org.lab.week03lab01.model.Album;
 import org.lab.week03lab01.model.Song;
 import org.lab.week03lab01.repository.AlbumRepository;
-import org.lab.week03lab01.repository.SongRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AlbumService {
     private final AlbumRepository albumRepository;
-    private final SongRepository songRepository;
+    private final SongService songService;
 
-    public AlbumService(AlbumRepository albumRepository, SongRepository songRepository){
+    public AlbumService(AlbumRepository albumRepository, SongService songService){
         this.albumRepository = albumRepository;
-        this.songRepository = songRepository;
+        this.songService = songService;
     }
 
     public Album createAlbum(Album newAlbum) {
@@ -22,7 +21,7 @@ public class AlbumService {
 
     public Album addSong(Long albumId, Long songId) {
         /// Add 404 custom exception
-        Song song = songRepository.findById(songId).orElseThrow();
+        Song song = songService.findById(songId);
         Album album = albumRepository.findById(albumId).orElseThrow();
 
         /// Add 409 custom exception when song is already in tha album songs array
