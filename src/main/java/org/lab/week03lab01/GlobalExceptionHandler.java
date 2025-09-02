3eb44251d30c47ebc1212f49332d186538cca8fc
Mauwt <david.jacobo@utec.edu.pe>
2025-09-02 16:26:19 -0500

    chore: add commented solutions

--- src/main/java/org/lab/week03lab01/GlobalExceptionHandler.java
+++ src/main/java/org/lab/week03lab01/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.lab.week03lab01;
 
+import org.lab.week03lab01.exceptions.ConflictException;
+import org.lab.week03lab01.exceptions.ResourceNotFoundException;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,4 +16,22 @@ public class GlobalExceptionHandler {
         problemDetail.setDetail(ex.getMessage());
         return problemDetail;
     }
+
+    /*
+    @ExceptionHandler({ConflictException.class})
+    public ProblemDetail conflictHandler(ConflictException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(409);
+        problemDetail.setTitle("Conflict Error");
+        problemDetail.setDetail(ex.getMessage());
+        return problemDetail;
+    }
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ProblemDetail notFoundHandler(ResourceNotFoundException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(404);
+        problemDetail.setTitle("Resource Not Found");
+        problemDetail.setDetail(ex.getMessage());
+        return problemDetail;
+    }
+     */
 }
---
+++ src/main/java/org/lab/week03lab01/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package org.lab.week03lab01.exceptions;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
